ed49dbe3610faccbb1c833f7d291783d78ef3ce0
Mark Brough <mark.brough@gmail.com>
2019-07-15 13:12:18 +0200

    Set title and description to something nicer

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,11 +9,11 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Publish What You Fund IATI Visualisation',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: "A simple (unofficial) visualisation to demonstrate the detail in Publish What You Fund's IATI data." }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
