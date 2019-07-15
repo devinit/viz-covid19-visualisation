92fa687d7719ff17c9991dc02cc0bf49d32ecfd9
Mark Brough <mark.brough@gmail.com>
2019-07-15 20:03:24 +0200

    Add progress bar and hide titles etc. during page load

--- pages/index.vue
+++ pages/index.vue
@@ -2,48 +2,53 @@
   <div class="container-fluid" style="margin-top: 20px;">
     <div>
       <h1>
-        Publish What You Fund IATI Data Visualisation <span class="loading-text" v-if="activityData == null">(Loading...)</span>
+        Publish What You Fund IATI Data Visualisation
       </h1>
       <div class="alert alert-warning">
         <b>Disclaimer:</b> This site is unofficial, based on <a href="http://publishwhatyoufund.org">Publish What You Fund</a>'s <a href="http://iatiregistry.org/publisher/pwyf">IATI data</a>.
       </div>
-      <p>The idea for this site was sparked by some debates about the appropriate granularity of open data and the limits of transparency. It can hopefully be seen as a contribution to those debates. The data published by Publish What You Fund about its own finances is possibly the most detailed and granular data released by any organisation. Presented here unchanged from the organisation's own IATI publication, it contains personnel costs aggregated by month, but otherwise every line item from the financial system is published. Revenue data is also published. The detailed expense classification (e.g. classification by salaries vs flights or hotel costs) is quite unique to Publish What You Fund's data; most other organisations publish much more aggregate data. The extent to which this approach could or should be replicated by other organisations is of course open for discussion.</p>
-      <h3>Summary</h3>
-      <b-form-group>
-        <b-form-radio-group
-          id="displaySummaryControls"
-          v-model="displaySummary"
-          :options="[
-            {'value': 'chart', 'text': 'Chart'},{'value': 'table', 'text': 'Table'}
-          ]"
-          name="displaySummaryControls"
-          buttons
-          button-variant="outline-primary"
-        ></b-form-radio-group>
-        <b-form-radio-group
-          id="transactionTypeControls"
-          v-model="transactionType"
-          :options="[
-            {'value': '4', 'text': 'Expenditure'},
-            {'value': '1', 'text': 'Revenue'}
-          ]"
-          name="transactionTypeControls"
-          buttons
-          button-variant="outline-dark"
-        ></b-form-radio-group>
-      </b-form-group>
-      <SummaryTable
-        v-if="activityData"
-        :activityData="activityData"
-        :displaySummary="displaySummary" />
-      <hr />
-      <h3>Activity detail</h3>
-      <ActivityTable
-        v-if="activityData"
-        v-for="activity in activityData"
-        :key="activity['iati-identifier']._text"
-        :activity="activity"
-        :transactionType="transactionType" />
+      <p class="lead">The idea for this site was sparked by some debates about the appropriate granularity of open data and the limits of transparency. It can hopefully be seen as a contribution to those debates. The data published by Publish What You Fund about its own finances is possibly the most detailed and granular data released by any organisation. Presented here unchanged from the organisation's own IATI publication, it contains personnel costs aggregated by month, but otherwise every line item from the financial system is published. Revenue data is also published. The detailed expense classification (e.g. classification by salaries vs flights or hotel costs) is quite unique to Publish What You Fund's data; most other organisations publish much more aggregate data. The extent to which this approach could or should be replicated by other organisations is of course open for discussion.</p>
+      <template v-if="activityData == null">
+        <h2 class="loading-text">(Loading data...)</h2>
+      </template>
+      <template v-if="activityData != null">
+        <h3>Summary</h3>
+        <b-form-group>
+          <b-form-radio-group
+            id="displaySummaryControls"
+            v-model="displaySummary"
+            :options="[
+              {'value': 'chart', 'text': 'Chart'},{'value': 'table', 'text': 'Table'}
+            ]"
+            name="displaySummaryControls"
+            buttons
+            button-variant="outline-primary"
+          ></b-form-radio-group>
+          <b-form-radio-group
+            id="transactionTypeControls"
+            v-model="transactionType"
+            :options="[
+              {'value': '4', 'text': 'Expenditure'},
+              {'value': '1', 'text': 'Revenue'}
+            ]"
+            name="transactionTypeControls"
+            buttons
+            button-variant="outline-dark"
+          ></b-form-radio-group>
+        </b-form-group>
+        <SummaryTable
+          v-if="activityData"
+          :activityData="activityData"
+          :displaySummary="displaySummary" />
+        <hr />
+        <h3>Activity detail</h3>
+        <ActivityTable
+          v-if="activityData"
+          v-for="activity in activityData"
+          :key="activity['iati-identifier']._text"
+          :activity="activity"
+          :transactionType="transactionType" />
+      </template>
     </div>
     <footer>
       <div class="row">
@@ -58,10 +63,13 @@
 <style>
 .loading-text {
   font-style: italic;
-  color: #cccccc;
+  color: #666;
 }
 #displaySummaryControls label, #transactionTypeControls label {
   cursor: pointer;
+}
+.nuxt-progress {
+  background-color: #666;
 }
 </style>
 <script>
@@ -69,6 +77,7 @@ import xmlJs from 'xml-js'
 import SummaryTable from '~/components/SummaryTable.vue'
 import ActivityTable from '~/components/ActivityTable.vue'
 export default {
+  loading: false,
   components: {
     ActivityTable,
     SummaryTable
@@ -106,7 +115,7 @@ export default {
         headers: {
           'X-Requested-With': 'XMLHttpRequest',
         },
-        progress: true
+        progress: false
       }).then(data => {
         this.activityData = xmlJs.xml2js(data, {compact: true})['iati-activities']['iati-activity'].map(
           activity => {
@@ -114,6 +123,7 @@ export default {
             return activity
           }
         )
+        this.$nuxt.$loading.finish()
       }).catch(error => {
         alert("Sorry, there was an error loading the data! Perhaps the IATI XML file moved or is currently unavailable?")
         console.log(error.response)
@@ -129,7 +139,10 @@ export default {
     }
   },
   mounted() {
-    this.loadData()
+    this.$nextTick(() => {
+      this.$nuxt.$loading.start()
+      this.loadData()
+    })
   }
 }
 </script>
