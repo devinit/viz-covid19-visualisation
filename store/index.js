4939b66fa87ca9dfbfd7eb57ab9721bda0c84a90
Mark Brough <mark.brough@gmail.com>
2020-04-16 14:38:38 +0200

    Set IATI source

--- store/index.js
+++ store/index.js
@@ -2,13 +2,17 @@ import axios from 'axios'
 import config from '../nuxt.config'
 
 export const state = () => ({
-  useCache: true,
+  useCache: false,
+  IATISource: 'dportal',
   CORSAnywhere: "https://cors-anywhere.herokuapp.com/"
 })
 
 export const mutations = {
   toggleUseCache(state) {
     state.useCache = !(state.useCache)
+  },
+  setIATISource(state, source) {
+    state.IATISource = source
   }
 }
 
