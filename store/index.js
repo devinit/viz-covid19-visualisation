d21429a9cee91d0b736e251116a9e41b10cf320c
Mark Brough <mark.brough@gmail.com>
2020-04-23 21:46:19 +0200

    Add to store

--- store/index.js
+++ store/index.js
@@ -1,10 +1,18 @@
 import axios from 'axios'
 import config from '../nuxt.config'
+import Vue from 'vue'
 
 export const state = () => ({
   useCache: false,
   IATISource: 'dportal',
-  CORSAnywhere: "https://cors-anywhere.herokuapp.com/"
+  CORSAnywhere: "https://cors-anywhere.herokuapp.com/",
+  contributions: [],
+  planRequirements: 0,
+  planFunded: 0,
+  codelists: {
+    countries: {}
+  },
+  originalActivityData: []
 })
 
 export const mutations = {
@@ -13,6 +21,21 @@ export const mutations = {
   },
   setIATISource(state, source) {
     state.IATISource = source
+  },
+  setContributions(state, contributions) {
+    state.contributions = contributions
+  },
+  setPlanRequirements(state, value) {
+    state.planRequirements = value
+  },
+  setPlanFunded(state, value) {
+    state.planFunded = value
+  },
+  setCodelists(state, codelists) {
+    Vue.set(state.codelists, 'countries', codelists)
+  },
+  setOriginalActivityData(state, activities) {
+    state.originalActivityData = activities
   }
 }
 
