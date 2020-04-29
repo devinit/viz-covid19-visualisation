4d30de600ce9d2c082829f69ad9a6f167bfd355e
Mark Brough <mark.brough@gmail.com>
2020-04-29 14:43:23 +0200

    Add shorter title

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <b-navbar toggleable="lg" type="dark" variant="primary">
-      <b-navbar-brand :to="'/'">{{ title }}</b-navbar-brand>
+      <b-navbar-brand :to="'/'">COVID-19 Response</b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
