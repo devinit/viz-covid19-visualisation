a5f4b6e5306725bf2471b5d94686aacbfcc5c22d
Mark Brough <mark.brough@gmail.com>
2021-02-10 00:58:39 +0100

    Remove unused imports, functions, variables from store

--- store/index.js
+++ store/index.js
@@ -1,11 +1,6 @@
-import axios from 'axios'
-import config from '../nuxt.config'
 import Vue from 'vue'
 
 export const state = () => ({
-  useCache: false,
-  IATISource: 'dportal',
-  CORSAnywhere: "https://cors-anywhere.herokuapp.com/",
   contributions: [],
   planRequirements: 0,
   planFunded: 0,
@@ -34,12 +29,6 @@ export const state = () => ({
 })
 
 export const mutations = {
-  toggleUseCache(state) {
-    state.useCache = !(state.useCache)
-  },
-  setIATISource(state, source) {
-    state.IATISource = source
-  },
   setContributions(state, contributions) {
     state.contributions = contributions
   },
